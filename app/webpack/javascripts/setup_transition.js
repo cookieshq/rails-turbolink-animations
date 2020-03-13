0ca3ebd35cfb4c985202d5d3508afac499d52fe8
matted.dev <info@matted.dev>
2020-03-13 11:41:46 +0000

    Set up basic animations to work on all pages

--- app/webpack/javascripts/setup_transition.js
+++ app/webpack/javascripts/setup_transition.js
@@ -1,85 +1,53 @@
-export default function setupTransition() {
+import runAnimations from './transition-animations/run-animations.js';
 
+export default function setupTransition() {
   let _isAnimating = false;
 
-  $(document).on('turbolinks:load', e => {
-    console.log('TURBOLINKS LOAD');
-    // Default load animation
-    _isAnimating = true;
-    $('main').fadeIn(3000, fadeInComplete);
-
-    function fadeInComplete() {
-      _isAnimating = false;
-    }
+  $(document).on('turbolinks:load', () => {
+    _isAnimating = false;
   });
-
 
   $(document).on('turbolinks:before-visit', e => {
     if (!_isAnimating) {
-      event.preventDefault();
-      event.stopImmediatePropagation();
-      _isAnimating = true;
-      const targetEl = e.target.activeElement;
-      const targetUrl = event.data.url;
-      console.log(e, targetEl);
+      const els = $('[class*=animate]');
+      removeAllAnimateClasses(els);
 
-      $('body, html').animate({scrollTop: 0}, 500);
+      const listToAnimate = $('[data-animate-out]');
+      if (listToAnimate.length > 0) {
+        console.log('Animating...');
+        _isAnimating = true;
+        // Prevent navigation
+        e.preventDefault();
+        // Get new url
+        const newUrl = event.data.url;
 
-      if ($(targetEl).hasClass('article-btn-link')) {
-        // Find closest img
-        const img = $(targetEl).siblings('.card-img')[0];
-        // Get height and position
-        const imgSpecs = {
-          width: $(img).width(),
-          height: $(img).height(),
-          top: $(img).offset().top,
-          left: $(img).offset().left,
-        }
-        // Clone node, add current position & append
-        const newImgNode = $(img).clone();
-        addCssToEl(newImgNode, imgSpecs);
-        newImgNode.appendTo('body');
+        // Run animations
+        runAnimations(listToAnimate, 'out');
 
-        console.log(newImgNode[0]);
-        // Add transition class
-        setTimeout(() => {
-          newImgNode.addClass('transition-to-fs');
-        }, 0);
- 
-        const targetUrl = event.data.url;
-        
-
-        $('main').fadeOut(3000, function() {
-          directToNewUrl(targetUrl);
+        $(document).one('animationEndout', () => {
+          console.log('All animations finished');
+          // ESlint says TB undefined
+          // eslint-disable-next-line
+          Turbolinks.visit(newUrl);
+          _isAnimating = false;
         });
-
-
       } else {
-        $('main').fadeOut(3000, function() {
-          directToNewUrl(targetUrl);
-        });
-
+        console.log('No elements to animate');
       }
-    }
-
-    function directToNewUrl(url) {
-      Turbolinks.visit(url);
+    } else {
+      console.log('Not animating');
     }
   });
 
+  $(document).on('turbolinks:before-cache', () => {
+    const els = $('[data-animate-out]');
 
-
-
-
-  $(document).on('turbolinks:before-cache', e => {
-    $('main').css({'opacity': '1'});
+    removeAllAnimateClasses(els);
   });
-
 }
 
-
-
-function addCssToEl(node, imgSpecs) {
-  node.css({'width': imgSpecs.width, 'height': imgSpecs.height, 'position': 'absolute', 'top': imgSpecs.top, 'left': imgSpecs.left });
+function removeAllAnimateClasses(els) {
+  $(els).removeClass(function(index, className) {
+    return (className.match(/(^|\s)animate-\S+/g) || []).join(' ');
+  });
 }
-
