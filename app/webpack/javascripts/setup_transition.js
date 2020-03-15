7b363fbad085c44165579e1aae5a49709c92693a
matted.dev <info@matted.dev>
2020-03-15 21:32:28 +0000

    [WIP] Separate running of animatinos for general & custom animations

--- app/webpack/javascripts/setup_transition.js
+++ app/webpack/javascripts/setup_transition.js
@@ -2,6 +2,8 @@ import runAnimations from './transition-animations/run-animations.js';
 
 export default function setupTransition() {
   let _isAnimating = false;
+  // Create list of el nodes to animate
+  let listToAnimate = [[], []];
 
   $(document).on('turbolinks:load', () => {
     _isAnimating = false;
@@ -9,42 +11,66 @@ export default function setupTransition() {
 
   $(document).on('turbolinks:before-visit', e => {
     if (!_isAnimating) {
+      _isAnimating = true;
+
+      // Prevent navigation
+      e.preventDefault();
+
+      // Get new url
+      const newUrl = event.data.url;
+
+      // Remove any trailing animation classes
       const els = $('[class*=animate]');
       removeAllAnimateClasses(els);
 
-      const listToAnimate = $('[data-animate-out]');
-      if (listToAnimate.length > 0) {
-        console.log('Animating...');
-        _isAnimating = true;
-        // Prevent navigation
-        e.preventDefault();
-        // Get new url
-        const newUrl = event.data.url;
+      // *** Custom animations *** \\
+      addCustomElToList(event);
 
-        // Run animations
-        runAnimations(listToAnimate, 'out');
+      // *** General animations *** \\
+      addGeneralElsToList();
 
-        $(document).one('animationEndout', () => {
-          console.log('All animations finished');
-          // ESlint says TB undefined
-          // eslint-disable-next-line
-          Turbolinks.visit(newUrl);
-          _isAnimating = false;
-        });
-      } else {
-        console.log('No elements to animate');
-      }
+      // Run animations
+      runAnimations(listToAnimate);
+
+      $(document).one('animationEndOut', () => {
+        console.log('ANIMATION END EVENT');
+        // ESlint says TB undefined
+        // eslint-disable-next-line
+        Turbolinks.visit(newUrl);
+        _isAnimating = false;
+      });
     } else {
       console.log('Not animating');
     }
   });
 
+  // Clean up function
   $(document).on('turbolinks:before-cache', () => {
     const els = $('[data-animate-out]');
-
     removeAllAnimateClasses(els);
   });
+
+  function addCustomElToList(event) {
+    // Get element that triggered page load
+    const el = event.target.activeElement;
+
+    if ($(el).attr('data-custom-animation')) {
+      listToAnimate[0].push(el);
+    }
+  }
+
+  function addGeneralElsToList() {
+    const generalAnimationList = $('[data-animate-out]');
+
+    if (generalAnimationList.length > 0) {
+      $(generalAnimationList).each((ind, el) => {
+        listToAnimate[1].push(el);
+      });
+    }
+  }
 }
+
+// Utility functions
 
 function removeAllAnimateClasses(els) {
   $(els).removeClass(function(index, className) {
--- app/webpack/javascripts/transition-animations/run-animations.js
+++ app/webpack/javascripts/transition-animations/run-animations.js
@@ -1,27 +1,48 @@
-export default function runAnimations(elList, type) {
-  let animationsFinished = 0;
-  const totalAnimations = elList.length;
+export default function runAnimations(elList) {
+  //let animationsFinished = 0;
+  const totalAnimations = elList.flat().length;
 
-  $(elList).each((ind, el) => {
-    $(el).addClass($(el).attr(`data-animate-${type}`));
+  console.log('El list is ', elList, totalAnimations);
+  // Fire custom animations
+  $(elList[0]).each((ind, el) => {
+    console.log(el);
+    //const animationName = $(el).attr('data-custom-animation');
+    const targetElName = $(el).attr('data-custom-animation-target');
+    const targetEl = $(`#${targetElName}`)[0];
 
-    $(el).one(
-      'animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd',
-      function() {
-        animationsFinished++;
-        checkForAllFinished(el);
-      }
-    );
+    // Get position of el to copy
+
+    // $(targetEl).addClass(animationName);
+
+    // $(targetEl).one(
+    //   'animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd',
+    //   function() {
+    //     animationsFinished++;
+    //     checkForAllFinished(el);
+    //   }
+    // );
+
+    console.log(targetEl);
+
+    //$(el).addClass($(el).attr(`data-animate-out`));
+
+    // $(el).one(
+    //   'animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd',
+    //   function() {
+    //     animationsFinished++;
+    //     checkForAllFinished(el);
+    //   }
+    // );
   });
 
-  function checkForAllFinished(el) {
-    if (animationsFinished === totalAnimations) {
-      const event = new CustomEvent(`animationEnd${type}`, {
-        bubbles: true,
-        cancelable: true
-      });
+  // function checkForAllFinished(el) {
+  //   if (animationsFinished === totalAnimations) {
+  //     const event = new CustomEvent(`animationEndout`, {
+  //       bubbles: true,
+  //       cancelable: true
+  //     });
 
-      el.dispatchEvent(event);
-    }
-  }
+  //     el.dispatchEvent(event);
+  //   }
+  // }
 }
